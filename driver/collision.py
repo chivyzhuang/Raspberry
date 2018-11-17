84fdb76522f765cdea3231df51aa7dc6d4fdbd34
ChivyZhuang <chivyzhuang@gmail.com>
2018-11-17 23:31:20 +0800

    合并正前方3个碰撞传感器事件

--- driver/collision.py
+++ driver/collision.py
@@ -5,7 +5,9 @@ INDEX_COLLISION_FRONT = 2
 INDEX_COLLISION_FRONT_RIGHT = 3
 
 collision_devices_map = {
-    INDEX_COLLISION_FRONT_LEFT: [DigitalInputDevice(25, pull_up=True)],
-    INDEX_COLLISION_FRONT: [DigitalInputDevice(8, pull_up=True), DigitalInputDevice(7, pull_up=True)],
+    INDEX_COLLISION_FRONT_LEFT: [],
+    INDEX_COLLISION_FRONT: [DigitalInputDevice(25, pull_up=True),
+                            DigitalInputDevice(8, pull_up=True),
+                            DigitalInputDevice(7, pull_up=True)],
     INDEX_COLLISION_FRONT_RIGHT: [DigitalInputDevice(1, pull_up=True)],
 }
