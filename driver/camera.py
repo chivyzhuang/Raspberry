5c30e8afd77fa2cb60733442545bb27f728da27e
ChivyZhuang <chivyzhuang@gmail.com>
2018-11-18 08:09:44 +0800

    翻转摄像头照片

--- driver/camera.py
+++ driver/camera.py
@@ -19,39 +19,39 @@ class Camera:
             return [], [], []
 
         # 第一次整图识别
-        f = open(self.save_path, 'rb')
-        qr = Image.open(f)
-        codes = zbarlight.scan_codes('qrcode', qr)
-        if codes is None:
-            codes = []
+        with Image.open(self.save_path) as qr:
+            qr = qr.rotate(180)
+            codes = zbarlight.scan_codes('qrcode', qr)
+            if codes is None:
+                codes = []
 
-        # 无二维码
-        if len(codes) == 0:
-            return [], [], []
+            # 无二维码
+            if len(codes) == 0:
+                return [], [], []
 
-        # 识别左边二维码
-        left_qr = qr.crop((360, 0, 720, 480))
-        left_codes = zbarlight.scan_codes('qrcode', left_qr)
-        if left_codes is None:
-            left_codes = []
+            # 识别左边二维码
+            left_qr = qr.crop((0, 0, 360, 480))
+            left_codes = zbarlight.scan_codes('qrcode', left_qr)
+            if left_codes is None:
+                left_codes = []
 
-        # 识别右边二维码
-        right_qr = qr.crop((0, 0, 360, 480))
-        right_codes = zbarlight.scan_codes('qrcode', right_qr)
-        if right_codes is None:
-            right_codes = []
+            # 识别右边二维码
+            right_qr = qr.crop((360, 0, 720, 480))
+            right_codes = zbarlight.scan_codes('qrcode', right_qr)
+            if right_codes is None:
+                right_codes = []
 
-        # 不在左右的放中间
-        center_codes = list(set(codes) - set(left_codes) - set(right_codes))
+            # 不在左右的放中间
+            center_codes = list(set(codes) - set(left_codes) - set(right_codes))
 
-        # 同时在左右的也放中间
-        for i in list(left_codes):
-            if i in right_codes:
-                left_codes.remove(i)
-                right_codes.remove(i)
-                center_codes.append(i)
+            # 同时在左右的也放中间
+            for i in list(left_codes):
+                if i in right_codes:
+                    left_codes.remove(i)
+                    right_codes.remove(i)
+                    center_codes.append(i)
 
-        return left_codes, center_codes, right_codes
+            return left_codes, center_codes, right_codes
 
 
 if __name__ == '__main__':
